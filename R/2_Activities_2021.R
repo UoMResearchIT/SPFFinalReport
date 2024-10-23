f953717e30b45bfa5c5cae71773bf942bb341812
Toni Price <toni.price@manchester.ac.uk>
2024-10-23 09:50:25 +0100

    Implement automated test for 2_Activities_2021.R (part of #42)
    For 2_Activities_2021.R:
    - Qualify occurrences of package functions
    - Save processed data as separate .rds file to facilitate testing
    - Implement test to ensure (saved) data produced by running code matches reference data

--- R/2_Activities_2021.R
+++ R/2_Activities_2021.R
@@ -7,14 +7,14 @@
 #'
 #' @examples
 #' \dontrun{
-#' get_activities()
+#' process_activities()
 #' }
 #'
 process_activities <- function(msoa_lim = NULL) {
 
   # Read population data
-  load("Data_ref/Processed/Population/pop_dat.RData")
-  load("Data_ref/Processed/TimeUseSurvey/tus_dat.RData")
+  pop_dat <- readRDS("Data_act/Processed/Population/pop_dat.rds")
+  tus_dat <- readRDS("Data_act/Processed/TimeUseSurvey/tus_dat.rds")
 
   # TEMP: Number of loops whilst getting the code working
   msoa_lim <- msoa_lim %||% 2
@@ -35,28 +35,28 @@ process_activities <- function(msoa_lim = NULL) {
     # Setting missings to NA
     dplyr::mutate(location_popular = ifelse(location %in% c(-9, 0, 10, 99), NA, location)) %>%
     # Merging on most popular location for each activity by strata
-    left_join(tus_dat %>%
-                # Only keeping non-missing locations
-                filter(!(location %in% c(-9, 0, 10, 99))) %>%
-                # Summarising by stratum and activity
-                dplyr::group_by_at(c('sex', 'agegr4', 'nssec5', 'daytype', 'activity', 'location')) %>%
-                dplyr::summarise(n = length(location))  %>%
-                dplyr::group_by_at(c('sex', 'agegr4', 'nssec5', 'daytype', 'activity')) %>%
-                # Getting proportion of time spent in each location by activity
-                dplyr::mutate(p = n/sum(n),
-                              tmp = max(p)) %>%
-                # Only keeping most popular activity
-                dplyr::filter(p == tmp) %>%
-                # Keeping first if more than one selected
-                dplyr::mutate(tmp = min(location)) %>%
-                dplyr::filter(location == tmp) %>%
-                # Ungrouping
-                dplyr::ungroup() %>%
-                # Only keeping relevant columns
-                dplyr::select_at(c('sex', 'agegr4', 'nssec5', 'daytype', 'activity', 'location')) %>%
-                # Renaming columns
-                dplyr::rename(tmp = location),
-              by = c('sex', 'agegr4', 'nssec5', 'daytype', 'activity'))  %>%
+    dplyr::left_join(tus_dat %>%
+                       # Only keeping non-missing locations
+                       dplyr::filter(!(location %in% c(-9, 0, 10, 99))) %>%
+                       # Summarising by stratum and activity
+                       dplyr::group_by_at(c('sex', 'agegr4', 'nssec5', 'daytype', 'activity', 'location')) %>%
+                       dplyr::summarise(n = length(location))  %>%
+                       dplyr::group_by_at(c('sex', 'agegr4', 'nssec5', 'daytype', 'activity')) %>%
+                       # Getting proportion of time spent in each location by activity
+                       dplyr::mutate(p = n/sum(n),
+                                     tmp = max(p)) %>%
+                       # Only keeping most popular activity
+                       dplyr::filter(p == tmp) %>%
+                       # Keeping first if more than one selected
+                       dplyr::mutate(tmp = min(location)) %>%
+                       dplyr::filter(location == tmp) %>%
+                       # Ungrouping
+                       dplyr::ungroup() %>%
+                       # Only keeping relevant columns
+                       dplyr::select_at(c('sex', 'agegr4', 'nssec5', 'daytype', 'activity', 'location')) %>%
+                       # Renaming columns
+                       dplyr::rename(tmp = location),
+                     by = c('sex', 'agegr4', 'nssec5', 'daytype', 'activity'))  %>%
     # Filling in the missings
     dplyr::mutate(location_popular = ifelse(is.na(location_popular),
                                             tmp,
@@ -64,28 +64,28 @@ process_activities <- function(msoa_lim = NULL) {
     # Removing uncessary columns
     dplyr::select(-c(tmp))  %>%
     # Merging on most popular location for each activity by strata
-    left_join(tus_dat %>%
-                # Only keeping non-missing locations
-                filter(!(location %in% c(-9, 0, 10, 99))) %>%
-                # Summarising by stratum and activity
-                dplyr::group_by_at(c('activity', 'location')) %>%
-                dplyr::summarise(n = length(location))  %>%
-                dplyr::group_by_at(c('activity')) %>%
-                # Getting proportion of time spent in each location by activity
-                dplyr::mutate(p = n/sum(n),
-                              tmp = max(p)) %>%
-                # Only keeping most popular activity
-                dplyr::filter(p == tmp) %>%
-                # Keeping first if more than one selected
-                dplyr::mutate(tmp = min(location)) %>%
-                dplyr::filter(location == tmp) %>%
-                # Ungrouping
-                dplyr::ungroup() %>%
-                # Only keeping relevant columns
-                dplyr::select_at(c('activity', 'location')) %>%
-                # Renaming columns
-                dplyr::rename(tmp = location),
-              by = c('activity'))  %>%
+    dplyr::left_join(tus_dat %>%
+                       # Only keeping non-missing locations
+                       dplyr::filter(!(location %in% c(-9, 0, 10, 99))) %>%
+                       # Summarising by stratum and activity
+                       dplyr::group_by_at(c('activity', 'location')) %>%
+                       dplyr::summarise(n = length(location))  %>%
+                       dplyr::group_by_at(c('activity')) %>%
+                       # Getting proportion of time spent in each location by activity
+                       dplyr::mutate(p = n/sum(n),
+                                     tmp = max(p)) %>%
+                       # Only keeping most popular activity
+                       dplyr::filter(p == tmp) %>%
+                       # Keeping first if more than one selected
+                       dplyr::mutate(tmp = min(location)) %>%
+                       dplyr::filter(location == tmp) %>%
+                       # Ungrouping
+                       dplyr::ungroup() %>%
+                       # Only keeping relevant columns
+                       dplyr::select_at(c('activity', 'location')) %>%
+                       # Renaming columns
+                       dplyr::rename(tmp = location),
+                     by = c('activity'))  %>%
     # Filling in the missings
     dplyr::mutate(location_popular = ifelse(is.na(location_popular),
                                             tmp,
@@ -93,10 +93,10 @@ process_activities <- function(msoa_lim = NULL) {
     # Removing uncessary columns
     dplyr::select(-c(tmp))%>%
     # Merging on location labels
-    left_join(read_csv("Data_ref/Raw/TimeUseSurvey/uktus_metadata_location.csv") %>%
-                dplyr::select(location_popular = location,
-                              location_popular_label = location_label),
-              by = 'location_popular')
+    dplyr::left_join(readr::read_csv("Data_ref/Raw/TimeUseSurvey/uktus_metadata_location.csv") %>%
+                       dplyr::select(location_popular = location,
+                                     location_popular_label = location_label),
+                     by = 'location_popular')
 
   ########################################################################
   ### Sampling activity sequences (Method 1) - Complete sequences only ###
@@ -118,34 +118,34 @@ process_activities <- function(msoa_lim = NULL) {
     # the end of the profiles into the next day
     activities_complete <- activities_complete %>%
       # Getting hour and resting date after 00:00
-      mutate(hour = (floor((time-1)/6) + 4) %% 24,
-             date = if_else(hour %in% 0:3, date + 1, date)) %>%
+      dplyr::mutate(hour = (floor((time-1)/6) + 4) %% 24,
+                    date = dplyr::if_else(hour %in% 0:3, date + 1, date)) %>%
       # Removign day information as we have to shift the day
       dplyr::select(-c(day, day_label, daytype, daytype_label, season, season_label)) %>%
       # Adding on day information
-      mutate(day_label = weekdays(date),
-             day = as.numeric(factor(weekdays(date), levels = c('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'))),
-             daytype = case_when(day %in% c(1,7) ~ 1,
-                                 day %in% 2:6 ~ 2),
-             daytype_label = case_when(day %in% c(1,7) ~ 'Weekend',
-                                       day %in% 2:6 ~ 'Weekday'),
-             season = case_when(month(date) %in% c(12, 1, 2) ~ 1,
-                                month(date) %in% c(3:5) ~ 2,
-                                month(date) %in% c(6:8) ~ 3,
-                                month(date) %in% c(9:11) ~ 4),
-             season_label = case_when(month(date) %in% c(12, 1, 2) ~ 'Winter',
-                                      month(date) %in% c(3:5) ~ 'Spring',
-                                      month(date) %in% c(6:8) ~ 'Summer',
-                                      month(date) %in% c(9:11) ~ 'Autumn')) %>%
+      dplyr::mutate(day_label = weekdays(date),
+                    day = as.numeric(factor(weekdays(date), levels = c('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'))),
+                    daytype = dplyr::case_when(day %in% c(1,7) ~ 1,
+                                               day %in% 2:6 ~ 2),
+                    daytype_label = dplyr::case_when(day %in% c(1,7) ~ 'Weekend',
+                                                     day %in% 2:6 ~ 'Weekday'),
+                    season = dplyr::case_when(lubridate::month(date) %in% c(12, 1, 2) ~ 1,
+                                              lubridate::month(date) %in% c(3:5) ~ 2,
+                                              lubridate::month(date) %in% c(6:8) ~ 3,
+                                              lubridate::month(date) %in% c(9:11) ~ 4),
+                    season_label = dplyr::case_when(lubridate::month(date) %in% c(12, 1, 2) ~ 'Winter',
+                                                    lubridate::month(date) %in% c(3:5) ~ 'Spring',
+                                                    lubridate::month(date) %in% c(6:8) ~ 'Summer',
+                                                    lubridate::month(date) %in% c(9:11) ~ 'Autumn')) %>%
       # Removing first day
       dplyr::filter(date >= as.Date('2020-12-01') & date <= as.Date('2021-12-31'))
 
     # Adding micro-environments to the dataset
     activities_complete <- activities_complete %>%
-      dplyr::mutate(micro_group = case_when(location %in% c(11:12) ~ "home",
-                                            location %in% c(13, 14, 15, 16, 17, 19, 20, 21) ~ "indoor",
-                                            location %in% c(18, 31, 32) ~ "outdoor",
-                                            location %in% c(30, 33:49, 90) ~ "transport"))
+      dplyr::mutate(micro_group = dplyr::case_when(location %in% c(11:12) ~ "home",
+                                                   location %in% c(13, 14, 15, 16, 17, 19, 20, 21) ~ "indoor",
+                                                   location %in% c(18, 31, 32) ~ "outdoor",
+                                                   location %in% c(30, 33:49, 90) ~ "transport"))
 
     # Aggregating to the hourly time series
     activities_complete <- activities_complete %>%
@@ -161,7 +161,7 @@ process_activities <- function(msoa_lim = NULL) {
       dplyr::select(-c(minutes, sample))
 
     # Saving datasets
-    save(activities_complete, file = paste('Output_act/CaseStudy2/Activities/activities_', k, '.RData', sep = ''))
+    saveRDS(activities_complete, file = paste('Output_act/CaseStudy2/Activities/activities_', k, '.rds', sep = ''))
 
     # Printing index
     print(k)
---
+++ tests/testthat/test-2_Activities_2021.R
@@ -0,0 +1,56 @@
+source(here::here("tests","testthat", "test-helpers.R"))
+
+test_that("activities 2021 data matches reference data", {
+
+  skip_on_cran()
+  skip_on_bioc()
+  skip_on_ci()
+
+  # Check system env vars to determine whether to run this high level data test
+  skip_if(!env_should_run_test("SPF_RUN_HLDT_2"),
+          glue::glue("'SPF_RUN_HLDT_2=false' and 'SPF_RUN_ALL_HLDT=false';",
+                     " to run this test, set one or both to true"))
+
+  pop_dat <- readRDS(here::here("Data_act/Processed/Population/pop_dat.rds"))
+
+  # Note: There are 346 areas (hence 346 tests to run)
+  areas <- sort(unique(pop_dat$area_id))
+
+  # TEMP: Temporarily limit no. of tests: test only 1st & last n
+  # Set n to a small integer value to limit the number of tests to 2*n
+  n <- NA
+  if (is.na(n)) {
+    test_areas <- areas
+  } else {
+    test_areas <- areas[c(1:n, (length(areas) - n + 1):length(areas))]
+  }
+
+  # Example area IDs:
+  #   c("E02000984", "E02000985", "E02006916", "E02006917")
+
+  for (k in test_areas) {
+
+    # Reference data
+    path1 <- paste("Output_ref/CaseStudy2/Activities/activities_", k, ".RData",
+                   sep = "")
+
+    # Data produced by running package code
+    path2 <- paste("Output_act/CaseStudy2/Activities/activities_", k, ".rds",
+                   sep = "")
+
+    # Example ref data path:
+    #   "Output_ref/CaseStudy2/Activities/activities_E02000984.RData"
+
+    # Example act data path:
+    #   "Output_act/CaseStudy2/Activities/activities_E02000984.rds"
+
+    # This should load 1 object 'activities_complete' into the current
+    # environment; it is a data frame
+    load(file = here::here(path1))
+
+    act_activities_complete <- readRDS(here::here(path2))
+
+    expect_equal(act_activities_complete, activities_complete)
+  }
+
+})
